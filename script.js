b95911372b78ac6462ed84d5ab94ae7b201062de
Paul Thomas <paulthomas2107@gmail.com>
2023-07-23 20:58:55 +0100

    More

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ class Player {
     context.fillRect(this.x, this.y, this.width, this.height);
   }
   update() {
-    this.x += this.speed;
+    if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+    if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
   }
 }
 
@@ -24,7 +25,16 @@ class Game {
     this.canvas = canvas;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+    this.keys = [];
     this.player = new Player(this);
+    // Event Listeners
+    window.addEventListener('keydown', (e) => {
+      if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+    });
+    window.addEventListener('keyup', (e) => {
+      const index = this.keys.indexOf(e.key);
+      if (index > -1) this.keys.splice(index, 1);
+    });
   }
   render(context) {
     this.player.draw(context);
