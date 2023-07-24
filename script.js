99f3c3d7590c6c3164bca5637f5f7054a36c787a
Paul Thomas <paulthomas2107@gmail.com>
2023-07-24 07:46:51 +0100

    More

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ class Player {
     this.height = 100;
     this.x = this.game.width * 0.5 - this.width * 0.5;
     this.y = this.game.height - this.height;
-    this.speed = 5;
+    this.speed = 10;
   }
   draw(context) {
     context.fillRect(this.x, this.y, this.width, this.height);
   }
   update() {
+    // Horizontal Moveent
     if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
     if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+    // Horizontal Boundary
+    if (this.x < 0) this.x = 0;
+    else if (this.x > this.game.width - this.width)
+      this.x = this.game.width - this.width;
   }
 }
 
